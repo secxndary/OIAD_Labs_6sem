840f80080aa4eeb8a5ee73daf0c1740d7e32e2b1
secxndary <valdaitsevv@mail.ru>
2023-03-21 23:25:05 +0300

    5 лаба нахуй))) заебись ноута не было 2 недели))

--- OIAD_Lab4/main.py
+++ OIAD_Lab4/main.py
@@ -1,7 +1,5 @@
-import sklearn
 import mglearn
 import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -17,41 +15,44 @@ X, y = mglearn.datasets.make_wave(n_samples=60)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 lr = LinearRegression().fit(X_train, y_train)
-print("lr.coef_: {}".format(lr.coef_))
-print("lr.intercept_: {}", format(lr.intercept_))
+print("\n================  WAVE  ===============")
+print("lr.coef_ (w): {}".format(lr.coef_))
+print("lr.intercept_ (b): {}".format(lr.intercept_))
 
 print("Правильность на обучающем наборе: {:.2f}".format(lr.score(X_train, y_train)))
-print("Правильность на тестовом наборе: 2f}".format(lr.score(X_test, y_test)))
+print("Правильность на тестовом наборе: {:.2f}".format(lr.score(X_test, y_test)))
 
 
 X, y = mglearn.datasets.load_extended_boston()
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 lr = LinearRegression().fit(X_train, y_train)
 
+print("\n===============  BOSTON  ===============")
 print("Правильность на обучающем наборе: {:.2f}".format(lr.score(X_train, y_train)))
-print("Правильность на тестовом наборе: 2f}".format(lr.score(X_test, y_test)))
+print("Правильность на тестовом наборе: {:.2f}".format(lr.score(X_test, y_test)))
 
 
 ridge = Ridge().fit(X_train, y_train)
+print("\n=========  BOSTON  REGULARIZED  ========")
 print("Правильность на обучающем наборе: {:.2f}".format(ridge.score(X_train, y_train)))
-print("Правильность на тестовом наборе: 2f}".format(ridge.score(X_test, y_test)))
+print("Правильность на тестовом наборе: {:.2f}".format(ridge.score(X_test, y_test)))
 
 ridge10 = Ridge(alpha=10).fit(X_train, y_train)
+print("\n==========  RIDGE ALPHA = 10  =========")
 print("Правильность на обучающем наборе: {:.2f}". format(ridge10.score(X_train, y_train)))
-print("Правильность на тестовом наборе: 2f}".format(ridge10.score(X_test, y_test)))
+print("Правильность на тестовом наборе: {:.2f}".format(ridge10.score(X_test, y_test)))
 
 ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
+print("\n==========  RIDGE ALPHA = 0.1  ========")
 print("Правильность на обучающем наборе: {:.2f}". format(ridge01.score(X_train, y_train)))
-print("Правильность на тестовом наборе: 2f}".format(ridge01.score(X_test, y_test)))
+print("Правильность на тестовом наборе: {:.2f}".format(ridge01.score(X_test, y_test)))
 
 
 plt.plot(ridge.coef_, 's', label="Гребневая регрессия alpha=1")
-plt.plot(ridge10.coef_, '^', "Гребневая регрессия alpha=10")
-plt.plot(ridge01.coef_, 'v', "Гребневая регрессия alpha=0.1")
-
+plt.plot(ridge10.coef_, '^', label="Гребневая регрессия alpha=10")
+plt.plot(ridge01.coef_, 'v', label="Гребневая регрессия alpha=0.1")
 plt.plot(lr.coef_, 'o', label="Линейная регрессия")
 plt.xlabel("Индекс коэффициента")
-
 plt.ylabel("Оценка коэффициента")
 plt.hlines(0, 0, len(lr.coef_))
 plt.ylim(-25, 25)
@@ -63,18 +64,21 @@ plt.show()
 
 
 lasso = Lasso().fit(X_train, y_train)
+print("\n================  LASSO  ================")
 print("Правильность на обучающем наборе: {:.2f}".format(lasso.score(X_train, y_train)))
-print("Правильность на контрольном наборе: {:.2f}".format(lasso.score(X_test, y_test)))
+print("Правильность на тестовом наборе: {:.2f}".format(lasso.score(X_test, y_test)))
 print("Количество использованных признаков: {}". format(np.sum(lasso.coef_ != 0)))
 
 lasso001 = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+print("\n=============  LASSO 0.01  ==============")
 print("Правильность на обучающем наборе: {:.2f}". format(lasso001.score(X_train, y_train)))
-print("Правильность на тестовом наборе: 2f}". format(lasso001.score(X_test, y_test)))
+print("Правильность на тестовом наборе: {:.2f}". format(lasso001.score(X_test, y_test)))
 print("Количество использованных признаков: {}".format(np.sum(lasso001.coef_ != 0)))
 
 lasso0001 = Lasso(alpha=0.0001, max_iter=100000).fit(X_train, y_train)
+print("\n============  LASSO 0.0001  =============")
 print("Правильность на обучающем наборе: {:.2f}". format(lasso0001.score(X_train, y_train)))
-print("Правильность на тестовом наборе: 2f}". format(lasso0001.score(X_test, y_test)))
+print("Правильность на тестовом наборе: {:.2f}". format(lasso0001.score(X_test, y_test)))
 print("Количество использованных признаков: {}".format(np. sum(lasso0001.coef_ != 0)))
 
 plt.plot(lasso.coef_, 's', label="Лассо alpha=1")
