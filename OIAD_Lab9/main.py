36671297d02955f8a81db40f527253042519afa0
secxndary <valdaitsevv@mail.ru>
2023-04-12 08:50:18 +0300

    Lab 9 code fix

--- OIAD_Lab9/main.py
+++ OIAD_Lab9/main.py
@@ -1,16 +1,17 @@
 import mglearn
 import matplotlib.pyplot as plt
 import numpy as np
-
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_moons
+from sklearn.datasets import load_breast_cancer
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+
+
 X, y = make_moons(n_samples=100, noise=0.25, random_state=3)
 
-
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
-forest = RandomForestClassifier(n_estimators=5, random_state=2)
+forest = RandomForestClassifier(n_estimators=5, random_state=2, n_jobs=-1)
 forest.fit(X_train, y_train)
 
 
@@ -24,36 +25,48 @@ mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
 plt.show()
 
 
-
-from sklearn.datasets import load_breast_cancer
 cancer = load_breast_cancer()
 X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
-forest = RandomForestClassifier(n_estimators=100, random_state=0)
+forest = RandomForestClassifier(n_estimators=100, random_state=0, n_jobs=-1)
 forest.fit(X_train, y_train)
+print("\n====== Breast Cancer (100 деревьев)")
 print("Правильность на обучающем наборе: {:.3f}".format(forest.score(X_train, y_train)))
-print ("Правильность на тестовом наборе: {:.3f}".format(forest.score(X_test, y_test)))
+print("Правильность на тестовом наборе: {:.3f}\n".format(forest.score(X_test, y_test)))
 
 
-def plot_feature_importances_cancer (model):
+def plot_feature_importances_cancer(model):
     n_features = cancer.data.shape[1]
     plt.barh(range(n_features), model. feature_importances_, align='center')
     plt.yticks(np.arange(n_features), cancer. feature_names)
     plt.xlabel("Важность признака")
-    plt.ylabel ("Признак:")
+    plt.ylabel("Признак:")
     plt.show()
+
 
 plot_feature_importances_cancer(forest)
 
 
-
-from sklearn.ensemble import GradientBoostingClassifier
-X_train, X_test, y_train, y_test = train_test_split(cancer.data,
-    cancer.target, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
 
 gbrt = GradientBoostingClassifier(random_state=0)
 gbrt.fit(X_train, y_train)
+print("\n====== Default")
 print("Правильность на обучающем наборе: {:.3f}".format(gbrt.score(X_train, y_train)))
-print ("Правильность на тестовом наборе: {:.3f}".format (gbrt.score(X_test, y_test)))
+print("Правильность на тестовом наборе: {:.3f}".format(gbrt.score(X_test, y_test)))
+
+
+gbrt = GradientBoostingClassifier(random_state=0, max_depth=1)
+gbrt.fit(X_train, y_train)
+print("\n====== Depth = 1")
+print("Правильность на обучающем наборе: {:.3f}".format(gbrt.score(X_train, y_train)))
+print("Правильность на тестовом наборе: {:.3f}".format(gbrt.score(X_test, y_test)))
+
+
+gbrt = GradientBoostingClassifier(random_state=0, learning_rate=0.01)
+gbrt.fit(X_train, y_train)
+print("\n====== Rate = 0.01")
+print("Правильность на обучающем наборе: {:.3f}".format(gbrt.score(X_train, y_train)))
+print("Правильность на тестовом наборе: {:.3f}\n".format(gbrt.score(X_test, y_test)))
 
 
 gbrt = GradientBoostingClassifier(random_state=0, max_depth=1)
@@ -67,5 +80,6 @@ def plot_feature_importances_cancer(model):
     plt.xlabel("Важность признака")
     plt.ylabel("Признак")
     plt.show()
+
 
 plot_feature_importances_cancer(gbrt)
